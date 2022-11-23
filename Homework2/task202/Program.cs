971ee8b395f68a8175afe91bcc176a2993b304c3
Anna Lyubushkina <anna.lyubush@gmail.com>
2022-11-23 17:02:34 +0300

    task is complited

---
+++ Homework2/task202/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num=new Random().Next(-10000,100000);
+Console.WriteLine(num);
+int result=0;
+
+if (num>99)
+{    
+while(result==0)
+{
+    if (num>99&&num<1000)
+    {
+     result=num%10;  
+    }
+        num/=10;
+}
+Console.WriteLine(result);
+}
+else if (num<-99)
+{   
+while(result==0)
+{
+    if (num<-99&&num>-1000)
+    {
+     result=num%10;  
+    }
+        num/=10;
+}
+Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+
+
+
