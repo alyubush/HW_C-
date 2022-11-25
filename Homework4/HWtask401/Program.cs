ecf6b6065fe44c139bee504c378f98de70237474
Anna Lyubushkina <anna.lyubush@gmail.com>
2022-11-25 15:09:30 +0300

    task 25 is complited

---
+++ Homework4/HWtask401/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int num1=new Random().Next(1,10);
+int num2=new Random().Next(1,7);
+
+int GetNumCube(int res)
+{
+    int result=1;
+for (int i = 1; i <= num2; i++)
+{
+result=result *num1;
+  
+}
+return result;
+}
+
+Console.WriteLine(num1+" в степени "+num2+" равно "+GetNumCube(num1));
---
+++ Homework4/HWtask402/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int num=new Random().Next(1,10000);
+
+int GetFiguresSum(int res)
+{
+    int sum=0;
+   while (res>0)
+    {
+        sum=sum+res%10;
+        res/=10;
+    }
+    return sum;
+    }
+    Console.Write("сумма цифр числа "+num+" равна "+GetFiguresSum(num));
