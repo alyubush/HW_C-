83847ec8803e21d11f4a5e92089f457b962ffbc5
Anna Lyubushkina <anna.lyubush@gmail.com>
2022-11-28 11:24:49 +0300

    task 34 is complited

---
+++ Homework5/task501/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+
+int [] GetArray (int size)
+{
+    int [] arr=new int [size];
+    for (int i = 0; i < size; i++)
+    {
+       arr [i]= new Random().Next (100,1000); 
+    }
+    return arr;
+}
+
+int []array=GetArray(10);
+Console.Write(String.Join(", ", array));
+Console.WriteLine();
+int count=0;
+for (int i = 0; i <array.Length; i++)
+{
+    if (array[i]%2==0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine("в массиве "+count+" четных чисел");
