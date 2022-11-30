803c6843a4c7115c1f846751d585fdbf761342bc
Anna Lyubushkina <anna.lyubush@gmail.com>
2022-11-30 20:19:56 +0300

    task 41 is done

---
+++ Homework7/task701/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("введите кол-во строчек   ");
+int rows=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите кол-во столбцов   ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix=new double[rows,columns]; 
+for (int i = 0; i < rows; i++)
+{
+    for (int j= 0; j < columns; j++)
+    {
+       matrix [i,j]=new Random().Next(0,12)+new Random().NextDouble();
+       matrix[i,j] = Math.Round(matrix[i,j], 2);
+       Console.Write(matrix[i,j]+"  "); 
+    }
+    Console.WriteLine ();
+}
