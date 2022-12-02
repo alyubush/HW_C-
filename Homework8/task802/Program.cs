c2f1ee8ce4a5fcea54f57d5266049a0f45eea919
Anna Lyubushkina <anna.lyubush@gmail.com>
2022-12-02 16:38:48 +0300

    task561 is done

---
+++ Homework8/task802/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+Console.WriteLine ("введите количество строк и столбцов   ");
+int row=Convert.ToInt32(Console.ReadLine());
+
+
+int [,] matrix=new int [row,row];
+
+    for (int i = 0; i <matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <matrix.GetLength(1); j++)
+        {
+            matrix [i,j]=new Random().Next (1,10);
+             Console.Write(matrix [i,j]+",  ");
+        }
+        Console.WriteLine();
+    }
+ int [] sum=new int [row];
+ 
+   for (int i = 0; i <matrix.GetLength(0); i++)
+    {
+               sum [i]=0;
+               for (int j = 0; j <matrix.GetLength(1); j++)
+        {
+            sum[i]+=matrix [i,j]; 
+        }
+    }
+    
+    int max=sum[0];
+    int maxI=0;
+    for (int i = 0; i < row; i++)
+    {
+        if (sum [i]>max)
+        {
+            max=sum [i];
+            maxI=i;
+        }
+    }
+    Console.WriteLine("Строка с инднесом "+maxI+" имеет max сумму равную "+max);
---
+++ Homework8/task803/Program.cs
@@ -0,0 +1,52 @@
+﻿// SeЗадача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.WriteLine ("введите количество строк и столбцов   ");
+int row=Convert.ToInt32(Console.ReadLine());
+int column=row;
+
+int [,] GetMatrix()
+{
+    int [,] matrix=new int [row,column];
+    for (int i = 0; i <matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <matrix.GetLength(1); j++)
+        {
+            matrix [i,j]=new Random().Next (1,10);
+                     }
+            }return matrix;
+    }
+
+void PrintMatrix(int [,] matrix)
+{
+     for (int i = 0; i <matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix [i,j]+",  ");
+        }
+        Console.WriteLine();
+    }
+}
+int [,] matrix1 = GetMatrix();
+PrintMatrix(matrix1);
+Console.WriteLine("___________");
+int [,] matrix2 = GetMatrix();
+PrintMatrix(matrix2);
+Console.WriteLine("___________");
+
+int [,] matrix3=new int [row,column];
+ 
+ for (int i = 0; i <matrix3.GetLength(0); i++)
+    {
+        for (int j = 0; j <matrix3.GetLength(1); j++)
+        {
+            matrix3 [i,j]=matrix1[i,j]*matrix2[i,j];
+         }
+    };
+PrintMatrix(matrix3);
